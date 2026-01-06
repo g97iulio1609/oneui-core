9108f61c4ca3971e7bd2e0745d7e73e4d3fd324d
g97iulio1609 <giulio97.leone@gmail.com>
2026-01-06 14:01:44 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @onecoach/ui-core
+ * 
+ * Core UI components for OneCoach applications.
+ * This package contains foundational UI elements like Button, Input, Card, etc.
+ */
+
+export const VERSION = '0.0.0';
